e4d060477088ca58d5e7106ec4a4183d80dae974
EshanSankar <eshansankar12@gmail.com>
2025-06-07 14:57:57 -0400

    Updated CMakeLists.txt

--- src/gstreamer_bridge_node.cpp
+++ src/gstreamer_bridge_node.cpp
@@ -1,18 +1,3 @@
-#include <gstreamer_bridge.hpp>
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "gstreamer_bridge_node");
-
-    nodelet::Loader nodelet;
-    nodelet::M_string remap(ros::names::getRemappings());
-    nodelet::V_string nargv;
-
-    nodelet.load(ros::this_node::getName(),
-                "gstreamer_ros_bridge/GStreamerRosBridge",
-                remap, nargv);
-
-    ros::spin();
+int main() {
     return 0;
 }
-
--- src/gstreamer_cam_node.cpp
+++ src/gstreamer_cam_node.cpp
@@ -1,17 +1,3 @@
-#include <gstreamer_cam.hpp>
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "gstreamer_cam_node");
-    gst_init(&argc, &argv);
-    nodelet::Loader nodelet;
-    nodelet::M_string remap(ros::names::getRemappings());
-    nodelet::V_string nargv;
-
-    nodelet.load(ros::this_node::getName(),
-                "gstreamer_ros_bridge/GStreamerCam",
-                remap, nargv);
-
-    ros::spin();
+int main() {
     return 0;
 }
